82edcb9df95f32f1bb03d0b438f1a84152087e57
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-02 00:06:49 +0200

    create server modal

--- components/navigation-sidebar.tsx
+++ components/navigation-sidebar.tsx
@@ -31,9 +31,9 @@ const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md mx-auto" />
-      <ScrollArea className="flex-1 w-full">
+      <ScrollArea className="flex-1 w-full ">
         {servers.map((server) => (
-          <div key={server.id}>
+          <div key={server.id} className="mb-4">
             <NavigationItem
               id={server.id}
               imageUrl={server.imageUrl}
