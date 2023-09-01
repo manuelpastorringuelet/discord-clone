c80f75730a34a6780baf50212b503be6c662ed23
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-01 18:52:39 +0200

    navigation sidebar

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,7 +22,6 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-  useFormField,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
