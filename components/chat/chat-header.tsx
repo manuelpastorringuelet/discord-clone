177f8f6ecac3c09cdde1005c8930516604848015
Manuel Pastor <pastorringuelet@gmail.com>
2023-11-13 21:29:00 +0100

    video calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,6 +4,8 @@ import MobileToggle from "@/components/mobile-toggle";
 import UserAvatar from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -12,7 +14,7 @@ interface ChatHeaderProps {
 }
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
-    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+    <div className="text-md flex h-12 items-center border-b-2 border-neutral-200 px-3 font-semibold dark:border-neutral-800">
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash
@@ -21,10 +23,11 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         />
       )}
       {type === "conversation" && (
-        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
-      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <p className="text-md font-semibold text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
